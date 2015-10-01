58de3d55267c5799994818f83798259bb1d21d88
Sean Hammond <git@seanh.cc>
2015-10-01 19:16:55 +0100

    Add a Chrome badge blacklist
    Add a blacklist of URLs that the Chrome extension's badge will never
    show a number for.
    
    This is done by adding a separate API for the Chrome extension to use to
    get its badge number, and this API just returns 0 for blacklisted URLs.
    Doing it this way means that the (currently hardcoded) blacklist is
    server-side, so we can move it into the database and then add an admin
    page for modifying it.

--- h/api/views.py
+++ h/api/views.py
@@ -100,6 +100,37 @@ def search(request):
     }
 
 
+@api_config(context=Root, name='badge')
+def badge(request):
+    """Return the number of public annotations on a given page.
+
+    This is for the number that's displayed on the Chrome extension's badge.
+
+    Certain pages are blacklisted so that the badge never shows a number on
+    those pages. The Chrome extension is oblivious to this, we just tell it
+    that there are 0 annotations.
+
+    """
+    # TODO: Make this blacklist configurable by admins, instead of hardcoding.
+    blacklist = [
+        'https://hypothes.is/stream',
+        'https://hypothes.is/login',
+        'https://hypothes.is/forgot_password',
+        'http://127.0.0.1:5000/'
+    ]
+
+    uri = request.params.get('uri')
+
+    if not uri:
+        raise httpexceptions.HTTPBadRequest()
+
+    if uri in blacklist:
+        return {'total': 0}
+
+    return {
+        'total': search_lib.search(request, {'uri': uri, 'limit': 0})['total']}
+
+
 @api_config(route_name='access_token')
 def access_token(request):
     """The OAuth 2 access token view."""

6e79696d5e5c502471da8d89aa0f2fa5353674c3
Nick Stenning <nick@whiteink.com>
2015-10-02 14:04:38 +0200

    Add autofocus support to form field rendering

--- h/claim/schemas.py
+++ h/claim/schemas.py
@@ -10,4 +10,5 @@ _ = i18n.TranslationString
 
 class UpdateAccountSchema(CSRFSchema):
     password = password_node(title=_('New password'),
-                             hint=_('at least two characters'))
+                             hint=_('at least two characters'),
+                             autofocus=True)
